8bfc5fed09ecac7948c898b55ff1a2c662a5cda4
fonmusic <red-strat@mail.ru>
2023-06-02 13:40:55 +0300

    Note and User models added

--- NotesApi/Models/Note.cs
+++ NotesApi/Models/Note.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace NotesApi.Models;
 
 public class Note
 {
-    
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }    
 }

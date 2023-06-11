8fb0ec66e5e5e59f9fc3f6064c147bdc038e1963
fonmusic <red-strat@mail.ru>
2023-06-12 02:26:06 +0300

    UserId migration

---
+++ NotesApi/Migrations/20230611232002_UsedId.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsedId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Notes",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Users_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notes_UserId",
+                table: "Notes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Notes");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "ID");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

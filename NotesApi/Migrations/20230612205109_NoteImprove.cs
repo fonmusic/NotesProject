62d3064acb013972d041f36de84f4aa51fb05362
fonmusic <red-strat@mail.ru>
2023-06-12 23:52:02 +0300

    NoteImpove migration

---
+++ NotesApi/Migrations/20230612205109_NoteImprove.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoteImprove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Notes",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Notes",
+                newName: "ID");
+        }
+    }
+}
